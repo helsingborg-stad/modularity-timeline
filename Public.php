4c416400d9c8e3d7361ee9f2c3e754680113e678
Thor Brink <thor.brink@helsingborg.se>
2024-02-26 08:15:39 +0000

    fix: replace global blade service

--- Public.php
+++ Public.php
@@ -1,22 +1,17 @@
 <?php
 
-use HelsingborgStad\GlobalBladeService\GlobalBladeService;
+use ComponentLibrary\Init as ComponentLibraryInit;
 
 if (!function_exists('modularity_timeline_render_blade_view')) {
     function modularity_timeline_render_blade_view($view, $data = [], $compress = true)
     {
-        $bladeEngine = GlobalBladeService::getInstance([
-            MODULARITY_TIMELINE_MODULE_VIEW_PATH
-        ]);
+        $componentLibrary = new ComponentLibraryInit([]);
+        $bladeEngine = $componentLibrary->getEngine();
+        $data = array_merge($data, array('errorMessage' => false));
+        $viewPath = MODULARITY_TIMELINE_MODULE_VIEW_PATH;
 
         try {
-            $markup = $bladeEngine->makeView(
-                $view,
-                array_merge(
-                    $data,
-                    array('errorMessage' => false)
-                )
-            )->render();
+            $markup = $bladeEngine->makeView($view, $data, [], $viewPath)->render();
         } catch (\Throwable $e) {
             $markup .= '<pre style="border: 3px solid #f00; padding: 10px;">';
             $markup .= '<strong>' . $e->getMessage() . '</strong>';
