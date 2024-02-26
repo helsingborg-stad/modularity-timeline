7c451f8b6132f57032cb989dcd399f63a7f6fd57
Thor Brink <thor.brink@helsingborg.se>
2024-02-26 09:16:18 +0100

    fix: replace global blade service (#5)

--- source/php/App.php
+++ source/php/App.php
@@ -10,9 +10,6 @@ class App
     {
         //Register module
         add_action('plugins_loaded', array($this, 'registerModule'));
-
-        // Add view paths
-        add_filter('Municipio/blade/view_paths', array($this, 'addViewPaths'), 1, 1);
     }
 
     /**
@@ -27,23 +24,5 @@ class App
                 'Timeline'
             );
         }
-    }
-
-    /**
-     * Add searchable blade template paths
-     * @param array  $array Template paths
-     * @return array        Modified template paths
-     */
-    public function addViewPaths($array)
-    {
-        // If child theme is active, insert plugin view path after child views path.
-        if (is_child_theme()) {
-            array_splice($array, 2, 0, array(MODULARITY_TIMELINE_VIEW_PATH));
-        } else {
-            // Add view path first in the list if child theme is not active.
-            array_unshift($array, MODULARITY_TIMELINE_VIEW_PATH);
-        }
-
-        return $array;
     }
 }
